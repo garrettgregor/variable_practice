dea078badbcc92ce32bf531f0307a59271b5afaf
Garrett Gregor <garrett.gregor@gmail.com>
2023-02-03 17:03:29 -0700

    Add integer variables

--- variables.rb
+++ variables.rb
@@ -1,4 +1,8 @@
 email = "helloworld@gmail.com"
 date = "mm/dd/yy"
 affirmation = "stay positive"
-favorite_book = "The Count of Monte Cristo"
+favorite_book = "The Count of Monte Cristo"'
+age = 37
+number_of_monitors = 3
+pens = 1
+pencils = 1
