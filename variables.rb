a9d461ab5f46cfb4ec54f8152c54860e3edaf663
Garrett Gregor <garrett.gregor@gmail.com>
2023-02-05 09:09:32 -0700

    Change string variables and run puts commands

--- variables.rb
+++ variables.rb
@@ -1,15 +1,25 @@
 email = "helloworld@gmail.com"
 date = "mm/dd/yy"
 affirmation = "stay positive"
-favorite_book = "The Count of Monte Cristo"'
+favorite_book = "The Count of Monte Cristo"
 age = 37
 number_of_monitors = 3
 pens = 1
 pencils = 1
-hours_on_phone = .0056666
+hours_on_phone = 0.01
 turing_money_spent = 1200.00
 turing_money_owed = 23800.00
 is_excited = true
 finished_session_1 = true
 partnered_up = true
 understands_everything = false
+email # "helloworld@gmail.com"
+email = "garrett.gregor@gmail.com"
+date # "mm/dd/yy"
+date = "dd/mm/yyyy"
+affirmation # "stay positive"
+affirmation = "try and try again"
+
+puts email
+puts date
+puts affirmation
